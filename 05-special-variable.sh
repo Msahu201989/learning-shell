a042bc222d759cfa20131e97d24ef86d1d3a9c80
Mukesh.Sahu <msahu201989@gmail.com>
2023-08-30 21:06:14 +0530

    Variables in log file

--- 05-special-variable.sh
+++ 05-special-variable.sh
@@ -1,6 +1,6 @@
-echo $0 #Script Name
-echo $1 # First Argument
-echo $2 # Second Argument
-echo $* # All Argument
-echo $@ # All Argument
-echo $#  # no of Argumernt
+echo $0
+echo $1
+echo $2
+echo $*
+echo $@
+echo $#
